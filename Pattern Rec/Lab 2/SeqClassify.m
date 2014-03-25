69c3fe7b271b9df6793d299cbfdcb2cf5b1928be
U-Jake-PC\Jake <Jake@Jake-PC.(none)>
2014-03-24 23:42:09 -0400

    Adds the SeqClassify function

--- Pattern Rec/Lab 2/SeqClassify.m
+++ Pattern Rec/Lab 2/SeqClassify.m
@@ -5,6 +5,9 @@ function [ result ] = SeqClassify( Garray, point )
     %It will return true if the point should be classified as class1,
     %and false if the point should be classified as class2.
     
+    %In the case that the number of classifiers was insufficient, we will
+    %take the first classifier in the Garray to be canon.
+    
     for gIndex=1:length(Garray)
         currentElement = Garray{gIndex};
         if (currentElement{2} == 0)
@@ -19,6 +22,18 @@ function [ result ] = SeqClassify( Garray, point )
                 result = 1;
                 return
             end
+        end
+        
+        %If we're out of options, and can't come to a decision,
+        %assume that the first classifier is correct
+        if (gIndex == length(Garray))
+            firstElement = Garray{1};
+            if(firstElement{1}(point(1),point(2)) >= 0)
+                result = 1;
+            else
+                result = 0;
+            end
+            return
         end
     end
 end
